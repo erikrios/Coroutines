b5fa0c65d378631fa3b5cab9e8a58d94dc4d38b2
erik1997 <erikriosetiawan49@gmail.com>
2019-08-04 09:05:24 +0700

    Initial commit

---
+++ src/main/kotlin/com/erikriosetiawan/Main.kt
@@ -0,0 +1,11 @@
+package com.erikriosetiawan
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    launch { delay(1000L)
+        println("Coroutines!")
+    }
+    println("Hello")
+    delay(2000L)
+}
